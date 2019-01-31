1edf86c3c11631728b11cf6b7302e88dae0de2d6
Yiran Ni <yirann@uw.edu>
2019-01-31 11:09:45 -0800

    Updates

---
+++ precache-manifest.c2b617b422b39f33e4b1b18bb4f52a16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "23b75d9dac360c1a51a5b5f6360a5e5e",
+    "url": "/easy-parking-seattle/static/media/purpose.23b75d9d.jpg"
+  },
+  {
+    "revision": "1338f5bbb190e9d786d7",
+    "url": "/easy-parking-seattle/static/css/main.b625bb11.chunk.css"
+  },
+  {
+    "revision": "4c689ade458cf120a5fe367280955579",
+    "url": "/easy-parking-seattle/static/media/nikhil.4c689ade.png"
+  },
+  {
+    "revision": "13d2156f38ffbe209984",
+    "url": "/easy-parking-seattle/static/js/1.13d2156f.chunk.js"
+  },
+  {
+    "revision": "679c893e28a3a0ed53fe",
+    "url": "/easy-parking-seattle/static/js/runtime~main.679c893e.js"
+  },
+  {
+    "revision": "7d25b1c304353d051ffd9d698708945f",
+    "url": "/easy-parking-seattle/static/media/background.7d25b1c3.jpg"
+  },
+  {
+    "revision": "1338f5bbb190e9d786d7",
+    "url": "/easy-parking-seattle/static/js/main.1338f5bb.chunk.js"
+  },
+  {
+    "revision": "820accc35e224066e3e6d27f76d6ae44",
+    "url": "/easy-parking-seattle/static/media/demi.820accc3.jpg"
+  },
+  {
+    "revision": "26b08b6a79645ba9fe9b2387281d32ad",
+    "url": "/easy-parking-seattle/static/media/yiran.26b08b6a.png"
+  },
+  {
+    "revision": "af037b33f50a5236278976dd82d134d5",
+    "url": "/easy-parking-seattle/static/media/leo.af037b33.jpg"
+  },
+  {
+    "revision": "13d2156f38ffbe209984",
+    "url": "/easy-parking-seattle/static/css/1.2c234dfb.chunk.css"
+  },
+  {
+    "revision": "1f6fbcf8d32af68078bf09a3a05e1bfa",
+    "url": "/easy-parking-seattle/index.html"
+  }
+];
